3d2e5afae475d2dbae2e4431e9082575532ff204
Giovanni Andrade <giovanni.andra.ramos@gmail.com>
2020-03-04 15:34:36 -0300

    estrutura base

---
+++ src/main/java/br/com/plataforma/pagamentos/plataformapagamentos/PlataformaPagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.plataforma.pagamentos.plataformapagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaPagamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/plataforma/pagamentos/plataformapagamentos/PlataformaPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.plataforma.pagamentos.plataformapagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
